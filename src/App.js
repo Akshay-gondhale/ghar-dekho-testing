6034d8209a9513adacb521512b6627fe36917ffe
Akshay <akshay.pajv@gmail.com>
2021-07-05 22:59:58 +0530

    registration screen done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-import Login from './screens/Login';
+// import Login from './screens/Login';
+import Registration from './screens/Registration';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Registration />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={7000}
+        newestOnTop
+        closeOnClick
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase"
+const firebaseConfig = {
+    apiKey: "AIzaSyAT2i73e371ZTC81l_R_30bddqE2V8y8hU",
+    authDomain: "ghardekho-c3029.firebaseapp.com",
+    databaseURL: "https://ghardekho-c3029-default-rtdb.firebaseio.com",
+    projectId: "ghardekho-c3029",
+    storageBucket: "ghardekho-c3029.appspot.com",
+    messagingSenderId: "1039504250371",
+    appId: "1:1039504250371:web:ffa0d6971b695ad2ada036",
+    measurementId: "G-JFYS0Q6002"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase;
