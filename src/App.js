2cd0154dc56f8aeb64d8229e6e025c947763b4ca
Akshay <akshay.pajv@gmail.com>
2021-07-28 15:37:27 +0530

    automatic deploy testing

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import PendingScreeen from "./screens/PendingScreen"
 
 axios.defaults.baseURL = 'https://ghar-dekho-backend.herokuapp.com';
 // axios.defaults.baseURL = 'http://localhost:4000';
+
 function App() {
   const isLoggedIn = useSelector(state => state.AuthReducer.isLoggedIn)
 
