63f3807add7d53538601bf7bdce8c594fdc479c2
Akshay <akshay.pajv@gmail.com>
2021-10-28 03:37:07 +0530

    fixed chat screen ui bug

--- src/utils/BaseApi.js
+++ src/utils/BaseApi.js
@@ -8,6 +8,7 @@ const ImageUrl = "https://storage.googleapis.com/ghardekho-c3029.appspot.com/"
 //socket io url
 const SocketUrl = "https://ghar-dekho-backend.herokuapp.com"
 // const SocketUrl = "localhost:4000"
+
 module.exports = {
     BaseApi,
     ImageUrl,
